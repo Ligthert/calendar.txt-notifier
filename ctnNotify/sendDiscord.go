8bd9a1284223165b6dac8b4d0c5cfcca188bfee3
Sacha Ligthert <sacha@ligthert.net>
2026-06-25 14:16:53 +0200

    Rename file. Only send to Discord if there is a calendar event.

---
+++ ctnNotify/sendDiscord.go
@@ -0,0 +1,29 @@
+package ctnNotify
+
+import (
+	"bytes"
+)
+
+// A place to chuck in all the Discord related functions.
+// I am using webhooks, because a constant application is out of scope for the time-being and rocketscience in itself at the moment.
+func (ctnNotify *CtnNotify) sendDiscord() {
+
+	bodyTemplate := "{\"content\": \"TEMPLATE\"}"
+
+	// Deal with Birthdays first
+	if len(ctnNotify.CtnConfig.BirthdayEvents) >= 1 {
+		birthdayMsg := ctnNotify.transformMarkdown(bodyTemplate, "birthday")
+		birthdayJSON := []byte(birthdayMsg)
+		birthdayBody := bytes.NewReader(birthdayJSON)
+		ctnNotify.sendWebhook(ctnNotify.CtnConfig.DiscordWebHookURL, birthdayBody)
+	}
+
+	// Deal with Calendar entries
+	if len(ctnNotify.CtnConfig.CalendarEvents) >= 1 {
+		calendarMsg := ctnNotify.transformMarkdown(bodyTemplate, "calendar")
+		calendarJSON := []byte(calendarMsg)
+		calendarBody := bytes.NewReader(calendarJSON)
+		ctnNotify.sendWebhook(ctnNotify.CtnConfig.DiscordWebHookURL, calendarBody)
+	}
+
+}
